496cea8a19a35411fce9e21acef14780c05721a5
Junseok <yalvz@naver.com>
2023-07-10 21:22:39 +0900

    first commit

---
+++ src/test/java/com/generation/www/GenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
