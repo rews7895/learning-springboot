023b47f1408fd6cc76fc12eed35c42295e2a1b48
박준석 <nile7895@hani.com>
2023-09-12 09:18:58 +0900

    7.2 블로그 글 목록 뷰 구현하기
    p.177

---
+++ src/main/java/com/generation/www/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package com.generation.www.dto;
+
+import com.generation.www.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
