19fe924a23d0097cc878c8b3d8d44c82ca917e15
박준석 <nile7895@hani.com>
2023-09-20 13:15:13 +0900

    9.2.3 리프레시 토큰 도메인 구현하기
    p.245

---
+++ src/main/java/com/generation/www/config/TokenAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.generation.www.config;
+
+import com.generation.www.config.jwt.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authrizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+
+        String token = getAccessToken(authrizationHeader);
+
+        if(tokenProvider.validToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(String authorizationHeader) {
+        // 값이 null이거나 Bearer로 시작하지 않으면 null을 반환
+        if(authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            return authorizationHeader.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
+}
