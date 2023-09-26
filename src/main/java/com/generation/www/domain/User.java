0de0c95d92ea16c7fa297d65c4fe04ee7732b1b3
Junseok <yalvz@naver.com>
2023-09-26 22:36:27 +0900

    10.3.3 OAuth2 서비스 구현하기
    p.275

--- src/main/java/com/generation/www/domain/User.java
+++ src/main/java/com/generation/www/domain/User.java
@@ -30,10 +30,14 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     // 권한 반환
@@ -79,5 +83,11 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         // 계정이 사용가능한지 확인하는 로직
         return true;    // true -> 사용 가능
+    }
+
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
